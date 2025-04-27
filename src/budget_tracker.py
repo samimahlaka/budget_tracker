a56fbc5bf1891b72a41723e8aa1cff5492023a0b
Mahlaka <samimahlaka@gmail.com>
2025-04-27 18:09:55 -0500

    'Connect Python to SQLite database

--- src/budget_tracker.py
+++ src/budget_tracker.py
@@ -1,5 +1,13 @@
+import sqlite3
+
+
 def main():
     print('Welcome to the budget tracker')
 
+    conn = sqlite3.connect('data/budget.db')
+    cursor = conn.cursor()
+    conn.close()
+
+    
 if __name__ == 'main':
     main()
