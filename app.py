e4cd22cacd6badb860dff30c435d602d232afd92
Mahlaka <samimahlaka@gmail.com>
2025-05-07 12:41:45 -0500

    Implement transaction update with prefilled form and custom styling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask ,request, render_template, redirect, url_for, flash
 import sqlite3
 from forms import transactionForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'dev123'
@@ -61,15 +62,53 @@ def transactions():
     return render_template('transaction.html', transactions=transactions, Total_Amount = total_spending)
 
 
-@app.route('/delete_transaction', methods = ['POST'])
-def delete_transaction():
+@app.route('/delete_transaction/<int:id>', methods = ['POST'])
+def delete_transaction(id):
     conn = sqlite3.connect('data/budget.db')
     cursor = conn.cursor()
-    delete_id = request.form['id']
-    cursor.execute('DELETE from transactions WHERE id = ?', (delete_id,))
+    #delete_id = request.form['id']
+    cursor.execute('DELETE from transactions WHERE id = ?', ( id,))
     conn.commit()
     conn.close()
     return redirect('/view_transactions')
+
+@app.route('/edit_transaction/<int:id>', methods = ['POST','GET'])
+def edit_transaction(id):
+    form = transactionForm()
+    conn = sqlite3.connect('data/budget.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM transactions WHERE id =?', (id,))
+    row = cursor.fetchone()
+
+    if not row:
+        flash("No such transaction exist")
+        return redirect("/view_transactions")
+    
+    if request.method == 'POST' and form.validate_on_submit():
+        date = form.date.data
+        description = form.description.data
+        amount = form.amount.data
+        cursor.execute('UPDATE transactions SET date = ?, description=?, amount=? WHERE id =?',(date,description,amount,id)) 
+        conn.commit()
+        conn.close()
+        flash("Transaction updated successfully")
+        return redirect('/view_transactions')
+    
+    form.date.data = datetime.strptime(row[1], '%Y-%m-%d').date()
+    form.description.data = row[2]
+    form.amount.data= row[3]
+    conn.close()
+
+    return(render_template('edit_transaction.html', form=form))
+
+       
+    
+   
+
+
+
+
+
 
 if __name__ == '__main__':
     create_table()
